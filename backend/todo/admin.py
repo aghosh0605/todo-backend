25f71c8bb6fc96e9a9af7916848d6beabd7b3082
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-06-03 17:09:45 +0530

    🔒️ Admin Panel Validation, View, Filter Fixed

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from todo.models import TodoModel
+from django.contrib.auth.models import Group
+from django import forms
+
 
 # Register your models here.
-admin.site.register(TodoModel)
+
+# admin.site.register(TodoModel)
+@admin.register(TodoModel)
+class TodoModelAdmin(admin.ModelAdmin):
+    list_display = ('title','description','due_date','status')
+    list_filter = ('title','status')
+    search_fields = ['title', 'description']
+admin.site.unregister(Group)
+
+admin.site.site_header = "Todo App"
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from taggit.managers import TaggableManager
 import uuid
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class TodoModel(models.Model):
@@ -11,9 +12,13 @@ class TodoModel(models.Model):
         ("v", "Overdue")
     ]
     id=models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100,validators=[
+            MinLengthValidator(5, 'the field must contain at least 5 characters')
+            ])
+    description = models.CharField(max_length=1000,validators=[
+            MinLengthValidator(10, 'the field must contain at least 10 characters')
+            ])
     due_date = models.DateField(blank=True,null=True)
     tag= TaggableManager(blank=True)
-    status = models.CharField(max_length=1, choices=STATUS_OPTIONS)
+    status = models.CharField(max_length=1, choices=STATUS_OPTIONS,default='O')
     timestamp = models.DateTimeField(auto_now_add=True)
